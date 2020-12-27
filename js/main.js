f521bcf34470891068b9414bd4e03493f7da3a87
Mingyu Kim <mingyu@mingyu.co.kr>
2020-12-27 16:45:53 +1300

    make countdown script look nice

--- js/main.js
+++ js/main.js
@@ -1,196 +1,194 @@
-;(function () {
-	
-	'use strict';
+;
+(function () {
 
-	// iPad and iPod detection	
-	var isiPad = function(){
-		return (navigator.platform.indexOf("iPad") != -1);
-	};
+    'use strict';
 
-
-	var isiPhone = function(){
-	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
-			(navigator.platform.indexOf("iPod") != -1)
-	    );
-	};
-
-//	// Main Menu Superfish
-//	var mainMenu = function() {
-//
-//		$('#fh5co-primary-menu').superfish({
-//			delay: 0,
-//			animation: {
-//				opacity: 'show'
-//			},
-//			speed: 'fast',
-//			cssArrows: true,
-//			disableHI: true
-//		});
-//
-//	};
-
-	// Parallax
-	var parallax = function() {
-		if ( !isiPad() || !isiPhone() ) {
-			$(window).stellar();
-		}
-	};
+    // iPad and iPod detection	
+    var isiPad = function () {
+        return (navigator.platform.indexOf("iPad") != -1);
+    };
 
 
-	// Offcanvas and cloning of the main menu
-	var offcanvas = function() {
+    var isiPhone = function () {
+        return (
+            (navigator.platform.indexOf("iPhone") != -1) ||
+            (navigator.platform.indexOf("iPod") != -1)
+        );
+    };
 
-		var $clone = $('#fh5co-menu-wrap').clone();
-		$clone.attr({
-			'id' : 'offcanvas-menu'
-		});
-		$clone.find('> ul').attr({
-			'class' : '',
-			'id' : ''
-		});
+    //	// Main Menu Superfish
+    //	var mainMenu = function() {
+    //
+    //		$('#fh5co-primary-menu').superfish({
+    //			delay: 0,
+    //			animation: {
+    //				opacity: 'show'
+    //			},
+    //			speed: 'fast',
+    //			cssArrows: true,
+    //			disableHI: true
+    //		});
+    //
+    //	};
 
-		$('#fh5co-page').prepend($clone);
-
-		// click the burger
-		$('.js-fh5co-nav-toggle').on('click', function(){
-
-			if ( $('body').hasClass('fh5co-offcanvas') ) {
-				$('body').removeClass('fh5co-offcanvas');
-			} else {
-				$('body').addClass('fh5co-offcanvas');
-			}
-			// event.preventDefault();
-
-		});
-
-		$('#offcanvas-menu').css('height', $(window).height());
-
-		$(window).resize(function(){
-			var w = $(window);
+    // Parallax
+    var parallax = function () {
+        if (!isiPad() || !isiPhone()) {
+            $(window).stellar();
+        }
+    };
 
 
-			$('#offcanvas-menu').css('height', w.height());
+    // Offcanvas and cloning of the main menu
+    var offcanvas = function () {
 
-			if ( w.width() > 769 ) {
-				if ( $('body').hasClass('fh5co-offcanvas') ) {
-					$('body').removeClass('fh5co-offcanvas');
-				}
-			}
+        var $clone = $('#fh5co-menu-wrap').clone();
+        $clone.attr({
+            'id': 'offcanvas-menu'
+        });
+        $clone.find('> ul').attr({
+            'class': '',
+            'id': ''
+        });
 
-		});	
+        $('#fh5co-page').prepend($clone);
 
-	}
+        // click the burger
+        $('.js-fh5co-nav-toggle').on('click', function () {
 
-	
+            if ($('body').hasClass('fh5co-offcanvas')) {
+                $('body').removeClass('fh5co-offcanvas');
+            } else {
+                $('body').addClass('fh5co-offcanvas');
+            }
+            // event.preventDefault();
 
-	// Click outside of the Mobile Menu
-	var mobileMenuOutsideClick = function() {
-		$(document).click(function (e) {
-	    var container = $("#offcanvas-menu, .js-fh5co-nav-toggle");
-	    if (!container.is(e.target) && container.has(e.target).length === 0) {
-	      if ( $('body').hasClass('fh5co-offcanvas') ) {
-				$('body').removeClass('fh5co-offcanvas');
-			}
-	    }
-		});
-	};
+        });
+
+        $('#offcanvas-menu').css('height', $(window).height());
+
+        $(window).resize(function () {
+            var w = $(window);
 
 
-	// Animations
+            $('#offcanvas-menu').css('height', w.height());
 
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
+            if (w.width() > 769) {
+                if ($('body').hasClass('fh5co-offcanvas')) {
+                    $('body').removeClass('fh5co-offcanvas');
+                }
+            }
 
-			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
-				i++;
+        });
 
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .animate-box.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							el.addClass('fadeInUp animated');
-							el.removeClass('item-animate');
-						},  k * 50, 'easeInOutExpo' );
-					});
-					
-				}, 100);
-				
-			}
-
-		} , { offset: '85%' } );
-	};
-	
-	var stickyBanner = function() {
-		var $stickyElement = $('.sticky-banner');
-		var sticky;
-		if ($stickyElement.length) {
-		  sticky = new Waypoint.Sticky({
-		      element: $stickyElement[0],
-		      offset: 0
-		  })
-		}
-	};
-
-	// Set the date we're counting down to
-	var countDownDate = new Date("Jan 9, 2021 13:00:00").getTime();
-
-	// Update the count down every 1 second
-	var x = setInterval(function() {
-
-	// Get todays date and time
-	var now = new Date().getTime();
-
-	// Find the distance between now an the count down date
-	var distance = countDownDate - now;
-
-	// Time calculations for days, hours, minutes and seconds
-	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-	// Display the result in an element with id="demo"
-    //document.getElementById("demo").innerHTML = days + " <small>days</small>" + hours + " <small>hours</small> " + minutes + " <small>minutes</small> " + seconds + " <small>seconds</small> ";
-//document.getElementById("MyElement").className += " MyClass";
-	// Display the result in an element with id="demo"
-	document.getElementById("days").innerHTML = days +" <small>days</small>";
-	document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
-	document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
-	document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
-    document.getElementById("over-message").innerHTML = "The Birthday party is Over";
-
-	// If the count down is finished, write some text 
-	if (distance < 0) {
-	 clearInterval(x);
-     if ( document.getElementById("day-timer").className.match(/(?:^|\s)hidden(?!\S)/) ){
-         
-     }else{
-       document.getElementById("day-timer").className += " hidden";   
-     }
-	} else {
-        if ( document.getElementById("time-over").className.match(/(?:^|\s)hidden(?!\S)/) ){
-         
-     }else{
-       document.getElementById("time-over").className += " hidden";   
-     }
     }
-	}, 1000);
 
-	// Document on load.
 
-	$(function(){
-//		mainMenu();
-		parallax();
-		offcanvas();
-//		mobileMenuOutsideClick();
-		contentWayPoint();
-		stickyBanner();
-	});
+
+    // Click outside of the Mobile Menu
+    var mobileMenuOutsideClick = function () {
+        $(document).click(function (e) {
+            var container = $("#offcanvas-menu, .js-fh5co-nav-toggle");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+                if ($('body').hasClass('fh5co-offcanvas')) {
+                    $('body').removeClass('fh5co-offcanvas');
+                }
+            }
+        });
+    };
+
+
+    // Animations
+
+    var contentWayPoint = function () {
+        var i = 0;
+        $('.animate-box').waypoint(function (direction) {
+
+            if (direction === 'down' && !$(this.element).hasClass('animated')) {
+
+                i++;
+
+                $(this.element).addClass('item-animate');
+                setTimeout(function () {
+
+                    $('body .animate-box.item-animate').each(function (k) {
+                        var el = $(this);
+                        setTimeout(function () {
+                            el.addClass('fadeInUp animated');
+                            el.removeClass('item-animate');
+                        }, k * 50, 'easeInOutExpo');
+                    });
+
+                }, 100);
+
+            }
+
+        }, {
+            offset: '85%'
+        });
+    };
+
+    var stickyBanner = function () {
+        var $stickyElement = $('.sticky-banner');
+        var sticky;
+        if ($stickyElement.length) {
+            sticky = new Waypoint.Sticky({
+                element: $stickyElement[0],
+                offset: 0
+            })
+        }
+    };
+
+    // Set the date we're counting down to
+    var countDownDate = new Date("Jan 9, 2021 13:00:00").getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+        // Get todays date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in an element with id="demo"
+        //document.getElementById("demo").innerHTML = days + " <small>days</small>" + hours + " <small>hours</small> " + minutes + " <small>minutes</small> " + seconds + " <small>seconds</small> ";
+        //document.getElementById("MyElement").className += " MyClass";
+        // Display the result in an element with id="demo"
+        document.getElementById("days").innerHTML = days + " <small>days</small>";
+        document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
+        document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
+        document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
+
+        // If the count down is finished, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            if (!document.getElementById("day-timer").className.match(/(?:^|\s)hidden(?!\S)/)) {
+                document.getElementById("day-timer").className += " hidden";
+            }
+        } else {
+            if (!document.getElementById("time-over").className.match(/(?:^|\s)hidden(?!\S)/)) {
+                document.getElementById("time-over").className += " hidden";
+            }
+        }
+    }, 1000);
+
+    // Document on load.
+
+    $(function () {
+        //		mainMenu();
+        parallax();
+        offcanvas();
+        //		mobileMenuOutsideClick();
+        contentWayPoint();
+        stickyBanner();
+    });
 
 
 }());
